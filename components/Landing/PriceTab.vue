5cca3b318aed0f8aa0931a40bc86b55c39611563
Jianqi Pan <jannchie@gmail.com>
2024-01-22 23:14:45 +0900

    :adhesive_bandage: fix(style): select plan

--- components/Landing/PriceTab.vue
+++ components/Landing/PriceTab.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const isAnuual = defineModel<boolean>()
-const selectedCls = 'bg-primary-container'
+const selectedCls = 'bg-primary-container text-white'
 const unselectedCls = 'hover:bg-surface-border-base'
 </script>
 
 <template>
-  <div class="flex gap-2 text-white children:min-w-24 children:cursor-pointer children:gap-2 children:rounded-full children:px-4 children:py-2 children:text-center children:transition-all children:duration-300 children:ease-in-out">
+  <div class="flex gap-2 children:min-w-24 children:cursor-pointer children:gap-2 children:rounded-full children:px-4 children:py-2 children:text-center children:transition-all children:duration-300 children:ease-in-out">
     <div
       :class="{
         [selectedCls]: !isAnuual,
