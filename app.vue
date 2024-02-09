bdf9cb370656e714b731d52eb71a63ec8f5b1286
Jianqi Pan <jannchie@gmail.com>
2024-02-09 23:36:47 +0900

    :adhesive_bandage: fix(theme): init style

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 const { data: user } = await fetchUser()
 provide('user', user)
-useHead({
-  script: [{ children: '!function() {var e = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches; t = localStorage.getItem("scheme") || "auto";("dark" === t || e && "light" !== t) ? document.documentElement.setAttribute("data-scheme", "dark") : document.documentElement.setAttribute("data-scheme", "light")}()' }],
-})
 </script>
 
 <template>
