a5af15dc2336381c7e29b3e7521047aba494696b
Jianqi Pan <jannchie@gmail.com>
2024-10-20 15:47:05 +0900

    refactor(pricetab): migrate div buttons to Btn component && adjust styles

--- components/Landing/PriceTab.vue
+++ components/Landing/PriceTab.vue
@@ -1,52 +1,54 @@
 <script setup lang="ts">
+import { Btn } from '@roku-ui/vue'
+
 const variant = defineModel<string>()
-const selectedCls = 'bg-primary-containerd text-white'
+const selectedCls = 'bg-primary-5 text-white'
 const unselectedCls = 'hover:bg-surface-border-base'
 const t = useI18N()
 </script>
 
 <template>
-  <div class="flex gap-2 children:min-w-24 children:cursor-pointer children:gap-2 children:rounded-xl children:px-4 children:py-1 children:text-center children:transition-all children:duration-300 children:ease-in-out">
-    <div
-      :class="{
-        [selectedCls]: variant === 'monthly',
-        [unselectedCls]: variant !== 'monthly',
-      }"
+  <div class="flex gap-2 children:min-w-24 children:cursor-pointer children:gap-2 children:rounded-xl children:px-4 children:py-1 children:text-center children:duration-100 children:ease-in-out">
+    <Btn
+      :variant="variant === 'monthly' ? 'filled' : 'default'"
+      class="!h-14"
+      size="lg"
       @click="() => variant = 'monthly'"
     >
-      {{ t.plan.monthly }}
-      <div class="text-xs">
-        {{ t.plan.mostFlexible }}
+      <div>
+        {{ t.plan.monthly }}
+        <div class="text-xs op-50">
+          {{ t.plan.mostFlexible }}
+        </div>
       </div>
-    </div>
-    <div
-      :class="{
-        [selectedCls]: variant === 'one-time',
-        [unselectedCls]: variant !== 'one-time',
-      }"
+    </Btn>
+    <Btn
+      :variant="variant === 'one-time' ? 'filled' : 'default'"
+      class="!h-14"
       @click="() => variant = 'one-time'"
     >
-      {{ t.plan.oneTime }}
-      <div class="text-xs">
-        <i class="i-ant-design-alipay-circle-outlined" />
-        <i class="i-ant-design-wechat-filled" />
-        <i class="i-entypo-social-paypal" />
+      <div>
+        {{ t.plan.oneTime }}
+        <div class="text-xs op-50">
+          <i class="i-ant-design-alipay-circle-outlined" />
+          <i class="i-ant-design-wechat-filled" />
+          <i class="i-entypo-social-paypal" />
+        </div>
       </div>
-    </div>
-    <div
-      class="flex flex-col gap-0!"
-      :class="{
-        [selectedCls]: variant === 'annual',
-        [unselectedCls]: variant !== 'annual',
-      }"
+    </Btn>
+    <Btn
+      class="!h-14"
+      :variant="variant === 'annual' ? 'filled' : 'default'"
       @click="() => variant = 'annual'"
     >
       <div>
-        {{ t.plan.yearly }}
+        <div>
+          {{ t.plan.yearly }}
+        </div>
+        <div class="text-xs op-50">
+          {{ t.plan.save25 }}
+        </div>
       </div>
-      <div class="text-xs">
-        {{ t.plan.save25 }}
-      </div>
-    </div>
+    </Btn>
   </div>
 </template>
