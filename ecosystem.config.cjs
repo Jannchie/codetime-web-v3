6f0a73cf1fd0aa91925ff64c9537e6ca3d8b08db
Jianqi Pan <jannchie@gmail.com>
2025-01-01 19:40:03 +0900

    chore(pm2-config)!: reduce instances from 2 to 1

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: 'CodetimeWebV3',
       port: '3000',
       exec_mode: 'cluster',
-      instances: 2,
+      instances: 1,
       script: './.output/server/index.mjs',
     },
   ],
