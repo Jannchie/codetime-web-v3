ba41f004b1b1474c20d3e46be38c69ac7649e499
Jianqi Pan <jannchie@gmail.com>
2025-05-19 21:09:15 +0900

    refactor(useLocale): simplify composition api return statements

--- composables/useLocale.ts
+++ composables/useLocale.ts
@@ -1,30 +1,26 @@
 export function useLocale() {
   const router = useRouter()
   const route = router.currentRoute
-  const locale = computed(() => {
-    return route.value.params.locale as string
-  })
-  return locale
+  return computed(() => {
+      return route.value.params.locale as string
+    });
 }
 
 export function useCurrentPath() {
   const router = useRouter()
   const route = router.currentRoute
-  const path = computed(() => {
-    return route.value.path
-  })
-  return path
+  return computed(() => {
+      return route.value.path
+    });
 }
 
 export function useCurrentTab(headerTabs: Ref<{ path: string, label: string, id: string }[]>) {
   const locale = useLocale()
   const currentPath = useCurrentPath()
-  const currentTab = computed(() => {
-    const normalizedCurrentPath = currentPath.value.replace(/\/$/, '')
-    const ctab = headerTabs.value.find((tab) => {
-      return `/${locale.value}${tab.path}` === normalizedCurrentPath
-    })
-    return ctab
-  })
-  return currentTab
+  return computed(() => {
+      const normalizedCurrentPath = currentPath.value.replace(/\/$/, '')
+      return headerTabs.value.find((tab) => {
+              return `/${locale.value}${tab.path}` === normalizedCurrentPath
+            });
+    });
 }
