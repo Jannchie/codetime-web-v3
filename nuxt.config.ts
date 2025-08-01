3d5d38a65d43c4944293ca97beaf6a25fa24e4c2
Jianqi Pan <jannchie@gmail.com>
2025-08-02 03:19:26 +0900

    feat(sentry-integration): add sentry integration in nuxt config && update dependencies in package and lock files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
       apiHost: 'https://api.codetime.dev',
       githubClientId: process.env.NUXT_PUBLIC_GITHUB_CLIENT_ID || '978fe1a6f0c5d12f5beb',
       googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID || '1020029657488-f66ubcmj6qqg4h4ptjk505ljmkv55jkv.apps.googleusercontent.com',
+      sentryDsn: process.env.NUXT_PUBLIC_SENTRY_DSN || 'https://3682972d2ab3f65b115e618182c7fa35@o4509038403911680.ingest.us.sentry.io/4509768911486976',
     },
   },
   site: {
@@ -47,6 +48,20 @@ export default defineNuxtConfig({
         'Share Tech Mono': true,
       },
     },
-  ], 'nuxt-og-image'],
+  ], 'nuxt-og-image', '@sentry/nuxt'],
+
+  sentry: {
+    dsn: process.env.NUXT_PUBLIC_SENTRY_DSN || 'https://3682972d2ab3f65b115e618182c7fa35@o4509038403911680.ingest.us.sentry.io/4509768911486976',
+    environment: process.env.NODE_ENV || 'development',
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
+    integrations: {
+      browserTracing: true,
+      replay: {
+        maskAllText: false,
+        blockAllMedia: false,
+      },
+    },
+  },
+
   compatibilityDate: '2024-10-13',
 })
